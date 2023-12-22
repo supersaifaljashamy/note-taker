f645bdaa8528d3cdce678f11411849e5d3b897b6
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-22 15:46:00 -0800

    navbar component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,11 @@
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/heroes";
+import { Logo } from "./_components/logo";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F] pt-40">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pt-6">
         <Heading />
         <Heroes />
       </div>
